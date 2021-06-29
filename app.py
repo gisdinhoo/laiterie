a5cfcbc10c83d55fdc5e66dde8b6fd64cb090503
gisdinhoo <gisdinhoo@gmail.com>
2021-06-29 15:16:49 +0000

    initial commit

---
+++ app.py
@@ -0,0 +1,26 @@
+from flask import Flask, request, jsonify
+from bson.json_util import dumps
+from flask_pymongo import PyMongo
+
+
+app = Flask(__name__)
+
+
+dbName = "sample_restaurants"
+dbPassword ="azertyuiop"
+dbUri = "mongodb+srv://ghislain:{}@cluster0.lavoh.mongodb.net/{}?retryWrites=true&w=majority".format(dbPassword, dbName)
+local = "mongodb://localhost:27017/company"
+app.config["MONGO_URI"] = dbUri
+
+mongo = PyMongo(app)
+
+@app.route("/restaurants", methods=["GET"])
+def restaurants():
+    restau = mongo.db.restaurants.find()
+    resp = dumps(restau)
+    return resp
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
+        
